37c5615aa644165e392a74d95382c9238db90c93
Asif Shahriar <asifs.cse@gmail.com>
2022-07-08 21:21:14 +0530

    barpol

---
+++ barpol.R
@@ -0,0 +1,3 @@
+marks =c(70,95,80,74,72)
+barplot(marks,main="Comparng marks of 5 subjects",xlab="Math",ylab="Subject",names.arg=c("English","Science","Math","Hist","Hindi"),
+col="darkred",horiz=FALSE)
