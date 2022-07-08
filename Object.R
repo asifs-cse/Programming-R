0cb615fcca8e8418049d2061b401e50951790505
Asif Shahriar <asifs.cse@gmail.com>
2022-07-08 21:10:23 +0530

    show memory object in R

---
+++ Object.R
@@ -0,0 +1,7 @@
+name ="Python";
+num1= 8
+num2 =1.5
+nums = c(10,20,30,40,50,60)
+print(ls())
+print("Details of the objet in memeory")
+print(ls.str)
---
+++ test.R
@@ -0,0 +1,4 @@
+marks = c(70, 95, 80, 74,72)
+barplot(marks,main = "Comparing marks of 5 subjects",xlab = "Marks",
+ylab = "Subject",names.arg = c("English", "Science", "Math.", "Hist.","Hindi"),
+col = "darkred",horiz = FALSE)
